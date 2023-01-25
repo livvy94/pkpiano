215c7c9db1e8313c5eeef5e40f87687110656b6d
livvy94 <haxxor199x@gmail.com>
2023-01-25 17:54:30 -0500

    Instrument.cs MakeHex

--- PK Piano/SPC File Editing/Instrument.cs
+++ PK Piano/SPC File Editing/Instrument.cs
@@ -1,4 +1,6 @@
-﻿namespace PK_Piano.SPC_File_Editing
+﻿using System.Collections.Generic;
+
+namespace PK_Piano.SPC_File_Editing
 {
     class Instrument
     {
@@ -22,8 +24,26 @@
         public override string ToString()
         {
             //This gets used in the ListBox
+            //This formatting is meant to match the formatting in EBMusEd's instrument viewer!
             string result = $"{Index:X2}: {ADSR1:X2} {ADSR2:X2} {GAIN:X2}  {TuningMultiplier:X2}{TuningSub:X2}";
             return result;
+        }
+
+        public byte[] MakeHex(List<Instrument> instruments)
+        {
+            var result = new List<byte>();
+
+            foreach (var instrument in instruments)
+            {
+                result.Add(instrument.Index);
+                result.Add(instrument.ADSR1);
+                result.Add(instrument.ADSR2);
+                result.Add(instrument.GAIN);
+                result.Add(instrument.TuningMultiplier);
+                result.Add(instrument.TuningSub);
+            }
+
+            return result.ToArray();
         }
     }
 
